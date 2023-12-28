b28e124e54dcc3c9419ab265feebeec12b7aae81
Stone <151933935+metahistory@users.noreply.github.com>
2023-12-28 08:52:20 +0100

    configure pwa plugin

---
+++ manifest.ts
@@ -0,0 +1,18 @@
+export const manifest = {
+   name: "Application",
+   short_name: "App",
+   description: "...",
+   theme_color: "#ffffff",
+   icons: [
+      {
+         src: "icon.png",
+         sizes: "192x192",
+         type: "image/png",
+      },
+      {
+         src: "icon.png",
+         sizes: "512x512",
+         type: "image/png",
+      },
+   ],
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
+import { VitePWA } from "vite-plugin-pwa"
+import { manifest } from "./manifest"
 
 export default defineConfig({
    envPrefix: "APP_",
-   plugins: [react()],
+   plugins: [
+      react(),
+      VitePWA({
+         mode: "development",
+         manifest,
+         registerType: "autoUpdate",
+         injectRegister: "auto",
+         devOptions: {
+            enabled: true,
+         },
+      }),
+   ],
 })
